1e90659ba447663201b8f119bb9c05e40852a61b
ShahMashudAlam <shahmashudalam@gmail.com>
2024-06-01 05:24:23 +0500

    test123

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+/*
 if (process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 
@@ -5,10 +7,43 @@ if (process.env.NODE_ENV !== 'production'){
 }
 
 
-/*
 
 
 
+
+
+const express = require('express')
+const app = express() 
+
+
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser:true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
+
+
+*/
+
+
+
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const express = require('express');
@@ -41,36 +76,3 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
-
-
-*/
-
-
-
-
-
-const express = require('express')
-const app = express() 
-
-
-const expressLayouts = require('express-ejs-layouts')
-
-const indexRouter = require('./routes/index')
-
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser:true})
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
-
-app.use('/', indexRouter)
-
-app.listen(process.env.PORT || 3000)
